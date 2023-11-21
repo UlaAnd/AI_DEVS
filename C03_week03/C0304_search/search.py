6ff64e6b8c5bb13ebceb2c000bedb338917e0927
Ula_Andrz <ulandrzejczak@gmail.com>
2023-11-21 10:56:59 +0100

    new tasks added

--- C03_week03/C0304_search/search.py
+++ C03_week03/C0304_search/search.py
@@ -13,13 +13,25 @@ from langchain.embeddings import OpenAIEmbeddings
 from dotenv import load_dotenv, find_dotenv
 
 
+def get_data():
+    url = "https://unknow.news/archiwum.json"
+    response = requests.get(url)
+    if response.status_code >= 200:
+        data = response.json()
+        content = response.content
+        selected_data = data[:300]
+        with open("./archiwum_selected2.json", "w") as json_file:
+            json.dump(content, json_file)
+    return selected_data
+
+
 class Task:
     def main_method(self):
         controller_api = ControllerApi()
-        token = controller_api.get_auth(task_name="scraper")
+        token = controller_api.get_auth(task_name="search")
         data = controller_api.get_task(token=token)
         question = data.get("question")
-        task_answer = self.get_answer(url=url, question=question)
+        task_answer = self.create_collection(question=question)
         controller_api.post_answer(task_answer=task_answer, token=token)
 
     def get_answer(self, url, question):
@@ -28,31 +40,87 @@ class Task:
         result = controller.get_completion(prompt=question, system=system)
         return result
 
-    def get_data(self):
-        url = "https://unknow.news/archiwum.json"
-        response = requests.get(url)
-        if response.status_code >= 200:
-            data = response.json()
-            selected_data = data[:300]
-            with open("./archiwum_selected.json", "w") as json_file:
-                json.dump(selected_data, json_file)
-        return selected_data
-
-    def create_collection(self):
+    def create_collection(self, question):
         load_dotenv(find_dotenv())
         MEMORY_PATH = "archiwum_selected.json"
         COLLECTION_NAME = "archiwum"
 
         qdrant = QdrantClient("localhost", port=6333)
         embeddings = OpenAIEmbeddings(openai_api_key=ula_key)
-        query = "Dalle-3"
+        query = question
         query_embedding = embeddings.embed_query(query)
         result = qdrant.get_collections()
+
+        indexed = None
+        for collection in result.collections:
+            if collection.name == COLLECTION_NAME:
+                indexed = collection
+                break
+
+        # Create collection if not exists
+        if not indexed:
+            qdrant.create_collection(
+                COLLECTION_NAME,
+                vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
+                on_disk_payload=True
+            )
+
+        collection_info = qdrant.get_collection(COLLECTION_NAME)
+
+        # Add data to collection if empty
+        if not collection_info.points_count:
+            # Read File
+            with open(MEMORY_PATH, 'r') as f:
+                memory = json.load(f)
+
+            documents_with_meta = []
+            for document in memory:
+                title = document['title']
+                content = document['info']
+                url = document['url']
+                document_with_meta = [
+                    {
+                        'source': COLLECTION_NAME,
+                        'title': title,
+                        'content': content,
+                        'url': url,
+                        'uuid': str(uuid.uuid4())
+                    }
+                ]
+                documents_with_meta.append(document_with_meta)
+            # Generate embeddings and index
+            points = []
+            for document in documents_with_meta[:300]:
+                # print(document)
+                embedding = embeddings.embed_query(document[0]['content'])
+                points.append({
+                    'id': document[0]['uuid'],
+                    'payload': document[0],
+                    'vector': embedding
+                })
+
+            qdrant.upsert(
+                collection_name=COLLECTION_NAME,
+                wait=True,
+                points=[
+                    PointStruct(id=point['id'], vector=point['vector'], payload=point['payload']) for point in points
+                ]
+            )
+
+
+        search_result = qdrant.search(
+            collection_name=COLLECTION_NAME,
+            query_vector=query_embedding,
+            query_filter=Filter(
+                must=[FieldCondition(key="source", match=MatchValue(value=COLLECTION_NAME))]
+            ),
+            limit=1,
+        )
+        return search_result[0].payload.get("url")
 
 
 
 if __name__ == '__main__':
     task = Task()
-    task.get_data()
-    # task.main_method()
-    # task.create_collections()
+    # get_data()
+    task.main_method()
---
+++ C03_week03/people.py
@@ -0,0 +1,71 @@
+import requests
+
+from ai_controller import OpenAiController
+from controller_api import ControllerApi
+import json
+
+
+class Task:
+    def main_method(self):
+        controller_api = ControllerApi()
+        token = controller_api.get_auth(task_name="people")
+        data = controller_api.get_task(token=token)
+        question = data.get("question")
+        # question = "Gdzie mieszka Krysia Ludek?"
+        task_answer = self.get_answer(question=question)
+        controller_api.post_answer(task_answer=task_answer, token=token)
+
+
+    def get_answer(self, question):
+        meta = get_meta(question=question)
+        system = f'Na podstawie informacji i ulubiongo koloru odpowiedz na pytanie o użytkowniuku: {meta[0]}'
+        coded_question = f'{meta[1]}'
+        controller2 = OpenAiController()
+        answer = controller2.get_completion(prompt=coded_question, system=system)
+        print(answer)
+        return answer
+
+
+def get_meta(question: str):
+    url = "https://zadania.aidevs.pl/data/people.json"
+    response = requests.get(url)
+    if response.status_code >= 200:
+        data = response.json()
+
+    meta_name = get_name(question)
+    for document in data:
+        imie = document['imie']
+        nazwisko = document['nazwisko']
+        o_mnie = document['o_mnie']
+        ulubiony_kolor = document['ulubiony_kolor']
+
+        if imie == meta_name['imie']:
+            if nazwisko == meta_name['nazwisko']:
+                answer_meta = [
+                    {
+                        'o_użytkowniku': o_mnie,
+                        'ulubiony_kolor': ulubiony_kolor
+                    }
+                ]
+                return [answer_meta, meta_name['pytanie']]
+
+
+def get_name(question: str):
+    system = ("Zwróć imię i nazwisko BEZ ZDROBNIEŃ, nie odpowiadaj na pytanie, w polu 'pytanie' zaszyfruj dane"
+                "przykład:"
+              "user: jaki kolor się podoba Krysi Ludek?"
+              "asystent : {'imie' : 'Krystyna', 'nazwisko': 'Ludek', 'pytanie' : 'jaki kolor się podoba użytkownikowi?'}"
+              "user: Ulubiony kolor Zdziś Bzik, to?"
+              "asystent : {'imie' : 'Zdzisław', 'nazwisko': 'Bzik', 'pytanie' : 'Ulubiony kolor  użytkownika, to?}"
+              )
+    controller = OpenAiController()
+    name = controller.get_completion(prompt=question, system=system)
+    json_name = json.loads(name.replace("'", "\""))
+    print(json_name)
+    return json_name
+
+
+if __name__ == '__main__':
+    task = Task()
+    # get_data()
+    task.main_method()
